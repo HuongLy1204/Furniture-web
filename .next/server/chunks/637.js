d9348759f1652e6507dafaf4f31b97ac01472a5e
nly1204 <nhly1204@gmail.com>
2022-04-09 11:05:16 +0700

    build

---
+++ .next/server/chunks/637.js
@@ -0,0 +1,74 @@
+"use strict";
+exports.id = 637;
+exports.ids = [637];
+exports.modules = {
+
+/***/ 5719:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "Z": () => (/* binding */ createEmotionCache)
+/* harmony export */ });
+/* harmony import */ var _emotion_cache__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(1913);
+/* harmony import */ var _emotion_cache__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_emotion_cache__WEBPACK_IMPORTED_MODULE_0__);
+
+// prepend: true moves MUI styles to the top of the <head> so they're loaded first.
+// It allows developers to easily override MUI styles with other styling solutions, like CSS modules.
+function createEmotionCache() {
+    return _emotion_cache__WEBPACK_IMPORTED_MODULE_0___default()({
+        key: "css",
+        prepend: true
+    });
+};
+
+
+/***/ }),
+
+/***/ 804:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "Z": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var _mui_material_styles__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(8442);
+/* harmony import */ var _mui_material_styles__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_mui_material_styles__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _mui_material_colors__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(5574);
+/* harmony import */ var _mui_material_colors__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_mui_material_colors__WEBPACK_IMPORTED_MODULE_1__);
+
+
+// Create a theme instance.
+const theme = (0,_mui_material_styles__WEBPACK_IMPORTED_MODULE_0__.createTheme)({
+    palette: {
+        primary: {
+            main: "#556cd6"
+        },
+        secondary: {
+            main: "#19857b"
+        },
+        error: {
+            main: _mui_material_colors__WEBPACK_IMPORTED_MODULE_1__.red.A400
+        }
+    },
+    components: {
+        MuiLink: {
+            defaultProps: {
+                underline: "none"
+            },
+            styleOverrides: {
+                root: {
+                    color: "black",
+                    "&:hover, &.active": {
+                        color: "orange"
+                    }
+                }
+            }
+        }
+    }
+});
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (theme);
+
+
+/***/ })
+
+};
+;
